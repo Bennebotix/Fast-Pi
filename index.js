773b2ea390028b211bb1f2f448de02915940b22f
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2022-03-19 19:42:40 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,72 @@
+/*
+
+Useful Functions 1.0
+
+
+
+
+------------------------------------------------
+Reload
+*/
+function reload() {
+  window.location = window.location;
+};
+/*
+Description:
+Just Reload The Page
+Usage:
+reload()
+------------------------------------------------
+Add Event Listener
+*/
+function ael(eventListenerType, eventListenerTarget, runCodeToFunction) {
+  eventListenerTarget.addEventListener(eventListenerType.toString(), runCodeToFunction(eventData));
+};
+/*
+Description:
+Add An Event Listener
+Usage:
+ael('{Type Of Event Listener Thet You Want To Add}', function(eventData){
+  {Your Code Here ("eventData" Is The Required Function Input)}
+})
+------------------------------------------------
+Set Interval
+*/
+function si(time, runCodeToFunction) {
+  setInterval(runCodeToFunction(data), time)
+}
+/*
+Description:
+Create A setInterval Function
+Usage:
+si({Time That You Wnat To Wait Before Runnting The Code Again}, function(data){
+  {Your Code Here ("data" Is The Required Function Input)}
+})
+------------------------------------------------
+Set Timeout
+*/
+function st(time, runCodeToFunction) {
+  setTimeout(runCodeToFunction(data), time)
+}
+/*
+Description:
+Create A setTimeout Function
+Usage:
+st({Time That You Wnat To Wait Before Runnting The Code}, function(data){
+  {Your Code Here ("data" Is The Required Function Input)}
+})
+------------------------------------------------
+Set Timeout
+*/
+function st(time, runCodeToFunction) {
+  setImmidiate(runCodeToFunction(data))
+}
+/*
+Description:
+Create A setImmidiate Function
+Usage:
+sii(function(data){
+  {Your Code Here ("data" Is The Required Function Input)}
+})
+------------------------------------------------
+*/
